efb023000b102b28a2c4457fb1c691c93f0f67c0
thisisyr <einstein94yr@hotmail.com>
2018-08-14 08:50:22 +0800

    Initialize

---
+++ build/webpack.prod.config.js
@@ -0,0 +1,71 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const cleanWebpackPlugin = require('clean-webpack-plugin');
+const merge = require('webpack-merge');
+const webpackBaseConfig = require('./webpack.base.config.js');
+const fs = require('fs');
+const path = require('path');
+const package = require('../package.json');
+
+fs.open('./build/env.js', 'w', function(err, fd) {
+    const buf = 'export default "production";';
+    fs.write(fd, buf, 0, buf.length, 0, function(err, written, buffer) {});
+});
+
+const prodUrlBase = '';
+
+module.exports = merge(webpackBaseConfig, {
+    output: {
+        publicPath: '/dist/',
+        filename: '[name].[hash].js',
+        chunkFilename: '[name].[hash].chunk.js'
+    },
+    plugins: [
+        new cleanWebpackPlugin(['dist/*'], {
+            root: path.resolve(__dirname, '../')
+        }),
+        new ExtractTextPlugin({
+            filename: '[name].[hash].css',
+            allChunks: true
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: ['vender-exten', 'vender-base'],
+            minChunks: Infinity
+        }),
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"prod"'
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        }),
+        new HtmlWebpackPlugin({
+            title: 'Rexroth ECO v' + package.version,
+            favicon: './td_icon.ico',
+            filename: '../index.html',
+            template: './src/template/index.ejs',
+            inject: false
+        }),
+        new CopyWebpackPlugin([
+            {
+                from: 'td_icon.ico'
+            },
+            {
+                from: 'src/styles/fonts',
+                to: 'fonts'
+            },
+            {
+                from: 'src/views/main-components/theme-switch/theme'
+            }
+        ], {
+            ignore: [
+                'text-editor.vue'
+            ]
+        })
+    ]
+});
